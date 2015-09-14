2dfa58fb219f246607aa320c36deddf77cecbdde
Sarah Turek <snturek@gmail.com>
2015-09-14 15:57:52 -0600

    attempt to seed on heroku

--- server/database.js
+++ server/database.js
@@ -9,7 +9,8 @@ var Exercise = new Schema({
 
 mongoose.model('exercises', Exercise);
 
-mongoose.connect('mongodb://heroku_5sbnw7x2:6g6b2hl6gl4nq19dsdpmho3k8f@ds029638.mongolab.com:29638/heroku_5sbnw7x2' || 'mongodb://localhost/crud-exercises');
+mongoose.connect( 'mongodb://heroku_5sbnw7x2:6g6b2hl6gl4nq19dsdpmho3k8f@ds029638.mongolab.com:29638/heroku_5sbnw7x2' || 'mongodb://localhost/crud-exercises');
 
 
 
+// mongodb://heroku_5sbnw7x2:6g6b2hl6gl4nq19dsdpmho3k8f@ds029638.mongolab.com:29638/heroku_5sbnw7x2' ||
